f8eb0194c6b0ee5887c99185259ca8460bbc776c
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2026-01-16 22:37:23 +0530

    Add new components and animations for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HeadingSection from "@/components/HeadingSection";
 import Gridcards from "@/components/GridCards";
+import { Whychooseus } from "@/components/Whychooseus";
+import { InfiniteMovingCardsDemo } from "@/components/InfinteCards";
 export default function Home() {
   return (
     <>
       <div className=" min-h-screen text-white font-sans bg-black">
         <HeadingSection />
         <Gridcards />
+        <Whychooseus />
+        <InfiniteMovingCardsDemo />
       </div>
     </>
   );
